0175b3033ad8a383fc03d13f739507ea9202c5cc
Victor da Rosa Cordova <50641705+victorcordova014@users.noreply.github.com>
2024-11-23 00:12:13 +0000

    adicionado paginas index e produtos index

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() { 
+    var texto = "Taynara Marques Gilioli, amor da minha vida, te amo teste muito, estou estudando mas vc não sai da minha cabeça kk"
+    return texto
+}
+
+export default Home;
