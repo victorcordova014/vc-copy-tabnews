7ec803f3c67cf9ee8853e1262bb8c72434cdedd3
Victor da Rosa Cordova <50641705+victorcordova014@users.noreply.github.com>
2024-11-24 22:49:25 +0000

    reformulado h1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() { 
     var texto = "Taynara Marques Gilioli, amor da minha vida, te amo teste muito, estou estudando mas vc não sai da minha cabeça kk"
-    return texto
+    return <h1>Taynara Marques Gilioli, amor da minha vida, te amo teste muito, estou estudando mas vc não sai da minha cabeça kk</h1>
 }
 
 export default Home;
